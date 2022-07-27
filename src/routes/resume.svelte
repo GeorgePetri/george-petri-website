c1df36f8c61fa1a4d91e2cc7c939458ff8ef50fa
George Petri <t8_8t@icloud.com>
2022-07-27 20:07:20 +0300

    update libs

---
+++ src/routes/resume.svelte
@@ -0,0 +1,9 @@
+<h1>George Petri</h1>
+<p>PetriGeorge@outlook.com</p>
+<p>https://www.linkedin.com/in/george-petri</p>
+<p>https://github.com/GeorgePetri</p>
+<p>0749096544</p>
+<p>Brasov, RO</p>
+<h2>WORK EXPERIENCE</h2>
+<h2>EDUCATION</h2>
+<h2>SKILLS, CERTIFICATIONS, INTERESTS</h2>
